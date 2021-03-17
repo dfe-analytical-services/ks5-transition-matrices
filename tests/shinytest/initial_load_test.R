879ebb25468bf9f764dc82b99cf994140aad8673
Katie Tehrani <katie.tehrani@education.gov.uk>
2021-03-17 15:15:25 +0000

    fix initial load shiny test

--- tests/shinytest/initial_load_test.R
+++ tests/shinytest/initial_load_test.R
@@ -1,12 +1,15 @@
-# app <- ShinyDriver$new("../../")
-# app$snapshotInit("initial_load_test", screenshot = FALSE)
-# 
-# app$snapshot()
+
+#library(shiny)
 
 
-
+# Initialize a ShinyDriver object using the app (point to the right directory)
 app <- ShinyDriver$new("../../")
 app$snapshotInit("initial_load_test", screenshot = FALSE)
 
 # 1. Does it load  -------------------------------------------------------------------------------------------------------------------
 app$snapshot()
+
+
+
+
+

ba885777b5fb9ff0187b68154052d9dee45683be
Chris Bennett <chris.bennett@education.gov.uk>
2024-01-10 12:08:08 +0000

    new data processing method change to rerun old data instead of append to old data

--- background_scripts/data_processing.R
+++ background_scripts/data_processing.R
@@ -17,144 +17,54 @@ library(readr)
 rm(list=ls())
 
 # -----------------------------------------------------------------------------------------------------------------------------
-# ---- Things to change between runs ----
+# ---- Source functions ----
 # -----------------------------------------------------------------------------------------------------------------------------
 
-tm_file = "[KS5_STATISTICS_RESTRICTED].[TM_2023].[TM_data_2023U]"
+source("./background_scripts/data_processing_func.R")
 
+# -----------------------------------------------------------------------------------------------------------------------------
+# ---- Things to change between runs ----
+# -----------------------------------------------------------------------------------------------------------------------------
 
 ancillary_save_path <- "//lonnetapp01/DSGA2/!!Secure Data/SFR/2023/KS5/November_2023/06_ancillary/"
 current_year <- "2023U"
 
 # -----------------------------------------------------------------------------------------------------------------------------
-# ---- Reading in the data from SQL tables ----
+# ---- Thing to add - Reading in the data from SQL tables and running function ----
 # -----------------------------------------------------------------------------------------------------------------------------
 
-
 # establish connection to server
-con <- DBI::dbConnect(odbc::odbc(), driver = "SQL Server",
-                      server = "VMT1PR-DHSQL02")
+con <- DBI::dbConnect(odbc::odbc(), driver = "SQL Server", server = "VMT1PR-DHSQL02")
 
 
-# Select data from SQL tables
+# Select data from SQL tables - need to add the current year for unamended run and change from U to A for the amended run
 
-tm_data_raw <- tbl(con, sql(paste("select * from", tm_file))) %>% collect()
+tm_data_raw_2023 <- tbl(con, sql("select * from [KS5_STATISTICS_RESTRICTED].[TM_2023].[TM_data_2023U]")) %>% collect()
+tm_data_raw_2022 <- tbl(con, sql("select * from [KS5_STATISTICS_RESTRICTED].[TM_2022].[TM_data_2022A]")) %>% collect()
 
 # disconnect
 DBI::dbDisconnect(con)
 
 
+# add current year to the function call to produce the processed data - only needs updating for unamended runs as 
+# the SQL update above deals with version
+
+processed_data_2023 <- TM_data_prod_func(tm_data_raw_2023, 2023)
+processed_data_2022 <- TM_data_prod_func(tm_data_raw_2022, 2022)
 
 
-# -----------------------------------------------------------------------------------------------------------------------------
-# ---- SORTING NAs ----
-# -----------------------------------------------------------------------------------------------------------------------------
+# This needs updating to make sure it is the current year data as it is used to produce the ancillary data
+# should only need updating for unamended run as with the above
 
-colSums(is.na(tm_data_raw)) %>% as.data.frame() 
-# NAs in prior band which need removing
-# NAs in subj because no subj code was assigned within the SQL production code
-# SUBJ is assigned by joining on the QUAL_SUBJ_LOOKUP, 
-# but some subjects are filtered out of the L3VA process if they are not entered in 5 or more institutions
-# for the TMs we don't mind this rule, so we've decided to leave them in but we will have to create new SUBJ codes
-# to ensure I'm not overwriting any existing SUBJ codes, I'm generating 3 digit random numbers (SUBJ is usually 5 digits)
-
-
-tm_data_subj_na <- tm_data_raw %>% 
-  filter(!(is.na(PRIOR_BAND)),
-         is.na(SUBJ)) %>%
-  group_by(Subject) %>%
-  mutate(SUBJ = sample(100:900,1))
-
-tm_data <- tm_data_raw %>%
-  filter(!(is.na(PRIOR_BAND)),
-         !(is.na(SUBJ))) %>%
-  bind_rows(tm_data_subj_na)
-  
-colSums(is.na(tm_data)) %>% as.data.frame() 
-
-
-
-# -----------------------------------------------------------------------------------------------------------------------------
-# ---- NUMBERS & PERCENTAGES CALCULATED ---- 
-# -----------------------------------------------------------------------------------------------------------------------------
-
-
-student_numbers <- tm_data %>% 
-  mutate(GRADE = case_when(GRADE == "Fail" & SUBLEVNO != 130 ~ "U",
-                           TRUE ~ GRADE),
-         SIZE = case_when(ASIZE == 0 ~ GSIZE, 
-                          TRUE ~ ASIZE),
-         QUAL_ID = paste0(SUBLEVNO, SUBJ, SIZE, gradeStructure),
-         ROW_ID = paste0(SUBLEVNO, SUBJ, SIZE, PRIOR_BAND, gradeStructure)) %>%
-  mutate(across(c(everything(), -total_students), ~as.character(.))) %>%
-  arrange(ROW_ID) %>% 
-  pivot_wider(names_from = GRADE, values_from = total_students)
-
-
-
-student_percentages <- student_numbers %>% 
-  janitor::adorn_percentages() %>%
-  mutate_if(is.numeric, function(x){round(x*100, 2)}) %>%
-  mutate_if(is.numeric, ~paste0(.x, "%"))
-
-
-
-
-
-# -----------------------------------------------------------------------------------------------------------------------------
-# ---- LOOKUP & FULL GRADE OPTIONS CALCULATED ---- 
-# -----------------------------------------------------------------------------------------------------------------------------
-
-
-qual_lookup <- tm_data %>%
-  select(Qual_Description, SUBLEVNO, Subject, SUBJ, ASIZE, GSIZE, gradeStructure) %>%
-  distinct() %>%
-  mutate(SIZE = case_when(ASIZE == 0 ~ GSIZE, 
-                          TRUE ~ ASIZE))
-
-
-grade_lookup_u <- tm_data %>%
-  select(SUBLEVNO, SUBJ, ASIZE, GSIZE, gradeStructure) %>%
-  distinct() %>%
-  mutate(GRADE = case_when(SUBLEVNO == 130 ~ "Fail",
-                           TRUE ~ "U")) 
-  
-grade_lookup_sep <- tm_data %>%
-  select(SUBLEVNO, SUBJ, ASIZE, GSIZE, gradeStructure) %>%
-  distinct() %>%
-  mutate(GRADE = gradeStructure) %>%
-  separate_rows(. , GRADE, sep = ",") 
-
-grade_lookup <- bind_rows(grade_lookup_u, grade_lookup_sep) %>%
-  arrange(SUBLEVNO, SUBJ, ASIZE, GSIZE, gradeStructure, GRADE) %>%
-  mutate(SIZE = case_when(ASIZE == 0 ~ GSIZE, 
-                          TRUE ~ ASIZE))
-
-
-
-# would like to sort numeric and character grades differently so that all grades go from low - high
-# for numeric this is ascending, for character this is descending
-# will need to split again based on numeric and character and then re-combine again
-grades_char <- grade_lookup %>%
-  mutate(char_grade_check = is.na(suppressWarnings(as.numeric(grade_lookup$GRADE)))) %>%  # gives extra column - True is character, False is number
-  filter(char_grade_check == TRUE) %>% 
-  arrange(desc(GRADE))
-
-grades_num <- grade_lookup %>%
-  mutate(char_grade_check = is.na(suppressWarnings(as.numeric(grade_lookup$GRADE)))) %>%  # gives extra column - True is character, False is number
-  filter(char_grade_check == FALSE) %>%
-  arrange(GRADE)
-
-grades_ordered_lookup <- bind_rows(grades_char, grades_num) %>%
-  arrange(SUBLEVNO)
-
+current_year_data <- processed_data_2023
 
 
 # -----------------------------------------------------------------------------------------------------------------------------
 # ---- ANCILLARY DATA FOR EES ---- 
 # -----------------------------------------------------------------------------------------------------------------------------
 
-ancillary_data_numbers <- student_numbers %>%
+ancillary_data_numbers <- current_year_data$student_numbers %>%
+  select(-ReportYr) %>% 
   pivot_longer(!c(Qual_Description, SUBLEVNO, Potential_Level, ASIZE, GSIZE, MAPPING, Subject, gradeStructure, PRIOR_BAND, SUBJ, SIZE, QUAL_ID, ROW_ID),
                names_to = "grade",
                values_to = "count") %>%
@@ -171,7 +81,8 @@ ancillary_data_numbers <- student_numbers %>%
   arrange(qualification_code, subject_code, size, grade_structure, prior_attainment_band, grade)
 
 
-ancillary_data_percentages <- student_percentages %>%
+ancillary_data_percentages <- current_year_data$student_percentages %>%
+  select(-ReportYr) %>% 
   pivot_longer(!c(Qual_Description, SUBLEVNO, Potential_Level, ASIZE, GSIZE, MAPPING, Subject, gradeStructure, PRIOR_BAND, SUBJ, SIZE, QUAL_ID, ROW_ID),
                names_to = "grade",
                values_to = "percentage") %>%
@@ -196,49 +107,30 @@ ancillary_data <- ancillary_data_numbers %>%
 write_csv(ancillary_data, paste0(ancillary_save_path, 'tm_numbers_percentages_', current_year, '.csv'))
 
 
-
-# -----------------------------------------------------------------------------------------------------------------------------
-# ---- add academic year to data ---- 
-# -----------------------------------------------------------------------------------------------------------------------------
-
-student_numbers <- student_numbers %>% 
-  mutate(ReportYr = 2023) %>% 
-  select(ReportYr, everything())
-
-student_percentages <- student_percentages %>% 
-  mutate(ReportYr = 2023) %>% 
-  select(ReportYr, everything())
-
-qual_lookup <- qual_lookup %>% 
-  mutate(ReportYr = 2023) %>% 
-  select(ReportYr, everything())
-
-grades_ordered_lookup <- grades_ordered_lookup %>% 
-  mutate(ReportYr = 2023) %>% 
-  select(ReportYr, everything())
-
-
-# grade_list %>% filter(SUBLEVNO == 253, SUBJ == 20596, ASIZE == 1)
-
-
 # -----------------------------------------------------------------------------------------------------------------------------
 # ---- Saving Data ---- 
 # -----------------------------------------------------------------------------------------------------------------------------
 
-student_numbers_old <- read_rds("./data/all_student_numbers.rds")
-student_percentages_old <- read_rds("./data/all_student_percentages.rds")
-qual_lookup_old <- read_rds("./data/qual_lookup.rds")
-grades_ordered_lookup_old <- read_rds("./data/grade_lookup.rds")
+# bind all the processed data
 
-student_numbers <- bind_rows(student_numbers, student_numbers_old)
-student_percentages <- bind_rows(student_percentages, student_percentages_old)
-qual_lookup <- bind_rows(qual_lookup, qual_lookup_old)
-grades_ordered_lookup <- bind_rows(grades_ordered_lookup, grades_ordered_lookup_old)
+student_numbers <- lapply(ls(pattern = "processed_data_", .GlobalEnv), function(x){
+  bind_rows(mget(x, .GlobalEnv)[[1]]["student_numbers"])}) %>% bind_rows()
+
+student_percentages <- lapply(ls(pattern = "processed_data_", .GlobalEnv), function(x){
+  bind_rows(mget(x, .GlobalEnv)[[1]]["student_percentages"])}) %>% bind_rows() 
+
+qual_lookup <- lapply(ls(pattern = "processed_data_", .GlobalEnv), function(x){
+  bind_rows(mget(x, .GlobalEnv)[[1]]["qual_lookup"])}) %>% bind_rows()
+
+grades_ordered_lookup <- lapply(ls(pattern = "processed_data_", .GlobalEnv), function(x){
+  bind_rows(mget(x, .GlobalEnv)[[1]]["grades_ordered_lookup"])}) %>% bind_rows()
+
+
+# save as rds files
 
 saveRDS(student_numbers, "./data/all_student_numbers.rds")
 saveRDS(student_percentages, "./data/all_student_percentages.rds")
 
 saveRDS(qual_lookup, "./data/qual_lookup.rds")
 saveRDS(grades_ordered_lookup, "./data/grade_lookup.rds")
-
 

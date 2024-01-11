f31881c7534747d339e4065be3c1d38df28fd86d
Chris Bennett <chris.bennett@education.gov.uk>
2024-01-11 15:22:25 +0000

    2023A data upload

--- background_scripts/data_processing.R
+++ background_scripts/data_processing.R
@@ -26,8 +26,8 @@ source("./background_scripts/data_processing_func.R")
 # ---- Things to change between runs ----
 # -----------------------------------------------------------------------------------------------------------------------------
 
-ancillary_save_path <- "//lonnetapp01/DSGA2/!!Secure Data/SFR/2023/KS5/November_2023/06_ancillary/"
-current_year <- "2023U"
+ancillary_save_path <- "//lonnetapp01/DSGA2/!!Secure Data/SFR/2023/KS5/February_2024/06_ancillary/"
+current_year <- "2023A"
 
 # -----------------------------------------------------------------------------------------------------------------------------
 # ---- Thing to add - Reading in the data from SQL tables and running function ----
@@ -39,7 +39,7 @@ con <- DBI::dbConnect(odbc::odbc(), driver = "SQL Server", server = "VMT1PR-DHSQ
 
 # Select data from SQL tables - need to add the current year for unamended run and change from U to A for the amended run
 
-tm_data_raw_2023 <- tbl(con, sql("select * from [KS5_STATISTICS_RESTRICTED].[TM_2023].[TM_data_2023U]")) %>% collect()
+tm_data_raw_2023 <- tbl(con, sql("select * from [KS5_STATISTICS_RESTRICTED].[TM_2023].[TM_data_2023A]")) %>% collect()
 tm_data_raw_2022 <- tbl(con, sql("select * from [KS5_STATISTICS_RESTRICTED].[TM_2022].[TM_data_2022A]")) %>% collect()
 
 # disconnect

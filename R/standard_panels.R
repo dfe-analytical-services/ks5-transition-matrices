19d2e573c9669dfe90f8bf5e5acd71cbedb7ac00
Rich Bielby <Richard.BIELBY@education.gov.uk>
2024-09-13 15:48:18 +0100

    Removing renv caching from deploy script

--- R/standard_panels.R
+++ R/standard_panels.R
@@ -36,38 +36,3 @@ accessibility_panel <- function() {
     )
   )
 }
-
-support_links <- function() {
-  tabPanel(
-    "Support and feedback",
-    gov_main_layout(
-      gov_row(
-        column(
-          width = 12,
-          h2("Give us feedback"),
-          "If you spot any errors or bugs while using this dashboard, please screenshot and email them to ",
-          a(href = "mailto:Attainment.STATISTICS@education.gov.uk", "Attainment.STATISTICS@education.gov.uk", .noWS = c("after")), ".",
-          br(),
-          h2("Find more information on the data"),
-          "The data used to produce the dashboard, along with methodological information can be found on ",
-          a(href = "https://explore-education-statistics.service.gov.uk/", "Explore Education Statistics", .noWS = c("after")),
-          ".",
-          br(),
-          h2("Contact us"),
-          "If you have questions about the dashboard or data within it, please contact us at ",
-          a(href = "mailto:Attainment.STATISTICS@education.gov.uk", "Attainment.STATISTICS@education.gov.uk", .noWS = c("after")), br(),
-          h2("See the source code"),
-          "The source code for this dashboard is available in our ",
-          a(href = "https://github.com/dfe-analytical-services/ks5-transition-matrices", "GitHub repository", .noWS = c("after")),
-          ".",
-          br(),
-          br(),
-          br(),
-          br(),
-          br(),
-          br()
-        )
-      )
-    )
-  )
-}
